b567687c10cac478a3c0ef3150ddcbc6274e7067
georgiyelbaf@gmail.com <georgiyelbaf@gmail.com>
2025-05-29 23:59:59 +0300

    stage 3 task6

--- src/test/java/stage03/Task06_ParameterizedJUnitTest.java
+++ src/test/java/stage03/Task06_ParameterizedJUnitTest.java
@@ -3,12 +3,22 @@ package stage03;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.*;
 import java.util.stream.Stream;
 
 public class Task06_ParameterizedJUnitTest {
+
+    public static List<Integer> removeDuplicate(List<Integer> list){
+        Set<Integer> deDub = new HashSet<>(list);
+        List<Integer> integers  = new ArrayList<>();
+        integers.addAll(deDub);
+        return integers;
+
+    }
+
 
     public static Stream<Arguments> arrArguments() {
         return Stream.of(
@@ -17,31 +27,50 @@ public class Task06_ParameterizedJUnitTest {
     }
     @ParameterizedTest
     @MethodSource("arrArguments")
-    void testWithMethodSource(Integer[] arrDub, Integer[] arrDeDub) {
+    void testWithMethodSource(Integer[] arrDub, Integer[] arrDeDup) {
         List<Integer> before = Arrays.asList(arrDub);
-        List<Integer> shouldDeDub = Arrays.asList(arrDeDub);
-        List<Integer> newBefore = dedup(before);
-        Assertions.assertEquals(shouldDeDub, newBefore);
+        List<Integer> shouldDeDup = Arrays.asList(arrDeDup);
+        List<Integer> newBefore = removeDuplicate(before);
+        Assertions.assertEquals(shouldDeDup, newBefore);
     }
 
 
 
-   // @ParameterizedTest
-  //  @CsvSource(value = {"1 2 3 2,1 3 2",
-    //        "4 4 4,4"})
-   // public void test(int[] arrDub, int[] arrDeDub){
-       // System.out.println((arrDub));
-        //System.out.println((arrDeDub));
+    @ParameterizedTest
+    @CsvSource(value = {"1 2 3 2,1 2 3",
+            "4 4 4,4"})
+    public void test(String arrDub, String arrDeDub){
+        System.out.println(arrDub);
+        System.out.println(arrDeDub);
+        List<Integer> listWithDuplicates = toListParseString(arrDub);
+        List<Integer> resultShouldDeDup = toListParseString(arrDeDub);
+        List<Integer> afterRemoveDup = removeDuplicate(listWithDuplicates);
 
-    //     System.out.println(Arrays.toString(arrDub));
-  //              System.out.println(Arrays.toString(arrDeDub));
-   // }
+        System.out.println(listWithDuplicates);
+        System.out.println(resultShouldDeDup);
+        System.out.println(afterRemoveDup);
 
-
-    public static List<Integer> dedup(List<Integer> list){
-        Set<Integer> deDub = new HashSet<>(list);
-        List<Integer> integers  = new ArrayList<>();
-        integers.addAll(deDub);
-        return integers;
+        Assertions.assertEquals(resultShouldDeDup, afterRemoveDup);
     }
+
+    public List<Integer> toListParseString(String arr){
+        String[] words = arr.split(" ");
+        List<Integer> arrInteger = new ArrayList<>();
+        for (String word : words) {
+            arrInteger.add(toInt(word));
+        }
+        return arrInteger;
+    }
+    public int toInt(String number){
+        try{
+            return Integer.parseInt(number);
+        } catch (Exception e) {
+            System.out.println("Ошибка при преобразовании числа");
+        }
+        return -999999999;
+    }
+
+
+
+
 }
